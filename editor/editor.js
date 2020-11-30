71f83e447c17722d15a75b4695dc5e66497b2d74
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-11-30 01:31:34 +0100

    Add files via upload

---
+++ editor/editor.js
@@ -0,0 +1,161 @@
+const rte_config = {
+  classes: ['rich-text-editor'],
+  tools: ['b', 'i', 'u', 'a', 'x'], // visible tool(s)
+  text: {
+    b: ['Bold', 'B', '⌘+B'],
+    i: ['Italic', 'I', '⌘+I'],
+    u: ['Underline', 'U', '⌘+U'],
+    a: ['Link', 'A', '⌘+L'],
+    x: ['Source', '&#x22ef;', '⌘+⇧+X']
+  },
+  tidy: true, // tidy HTML output?
+  enter: true, // set to `false` to automatically submit the closest form on enter key press
+};
+
+const editor = document.querySelector('#editor-content');
+
+/* --- Message Center --- */
+
+window.addEventListener('message', receiveMessage, false);
+
+function receiveMessage(event) {
+
+  let data = JSON.parse(event.data);
+
+  document.querySelector('#save').style.display = 'block';
+
+  if (data.msg == 'clicked') {
+
+    console.log('clicked');
+
+    let type = data.type;
+    let value = data.value;
+    var reply = {};
+    reply.msg = 'updated';
+
+
+    if (type == 'text') {
+
+      editor.innerHTML = `<input type="text" class="form-control" value="${value}" id="input">`;
+
+      document.querySelector('#input').addEventListener('keyup', function(e) {
+        reply.value = document.querySelector('#input').value;
+        reply.type = 'text';
+        sendMessage(reply);
+      })
+
+    }
+
+    if (type == 'html') {
+      editor.innerHTML = `<textarea class="form-control" id="input">${value}</textarea>`;
+
+      new RTE(document.getElementById('input'), rte_config);
+
+      document.querySelector('.rich-text-editor-view').addEventListener('keyup', function(e) {
+        reply.value = document.querySelector('.rich-text-editor-view').innerHTML;
+        reply.type = 'html';
+        sendMessage(reply);
+      })
+
+    }
+
+    if (type == 'src') {
+
+      editor.innerHTML = `
+    <input type="file" id="fileInput" accept="image/*" id="fileInput" />
+    <button class="btn btn-outline-dark" id="chooseImage">Choose Image</button>
+    `;
+
+      document.querySelector('#chooseImage').addEventListener('click', function() {
+        document.querySelector('#fileInput').click();
+      })
+
+      document.getElementById('fileInput').addEventListener('change', uploadHandler);
+
+    }
+  }
+
+}
+
+// send the updated content
+function sendMessage(data) {
+  document.getElementById('iframe').contentWindow.postMessage(JSON.stringify(data), '*')
+}
+
+function uploadHandler(e) {
+
+  var width = 800;
+  var img = new Image();
+  img.onload = function() {
+    var canvas = document.createElement('canvas'),
+      ctx = canvas.getContext("2d"),
+      oc = document.createElement('canvas'),
+      octx = oc.getContext('2d');
+
+    canvas.width = width; // destination canvas size
+    canvas.height = canvas.width * img.height / img.width;
+
+    var cur = {
+      width: Math.floor(img.width * 0.5),
+      height: Math.floor(img.height * 0.5)
+    }
+
+    oc.width = cur.width;
+    oc.height = cur.height;
+
+    octx.drawImage(img, 0, 0, cur.width, cur.height);
+
+    while (cur.width * 0.5 > width) {
+      cur = {
+        width: Math.floor(cur.width * 0.5),
+        height: Math.floor(cur.height * 0.5)
+      };
+      octx.drawImage(oc, 0, 0, cur.width * 2, cur.height * 2, 0, 0, cur.width, cur.height);
+    }
+
+    ctx.drawImage(oc, 0, 0, cur.width, cur.height, 0, 0, canvas.width, canvas.height);
+    var base64Image = canvas.toDataURL('image/png')
+
+    console.log(base64Image);
+
+    var reply = {};
+    reply.msg = 'updated';
+    reply.value = base64Image;
+    reply.type = 'src';
+    sendMessage(reply);
+
+
+    /*
+        if (user) {
+
+          fetch("api/save", {
+              headers: {
+                  authorization: user.authToken,
+                  "Content-Type": "application/json",
+              },
+              method: 'post',
+              body: JSON.stringify({type: 'image', data: base64Image}),
+            }).then(function(response) {
+              return response.json();
+            }).then(function(mydata) {
+              data.entries[index][key] = mydata.filename;
+            })
+            .catch(function(error) {
+              console.log(error);
+            });
+        }
+    */
+
+    // cleaning up
+    URL.revokeObjectURL(img.src)
+
+  }
+  img.src = URL.createObjectURL(e.target.files[0]);
+
+}
+
+document.querySelector('#save').addEventListener('click', function() {
+  let data = {};
+  data.msg = 'save';
+  sendMessage(data);
+})
---
+++ editor/remote.js
@@ -0,0 +1,94 @@
+cfg = {};
+cfg.img_width = 800;
+cfg.remote_domain = '*';
+cfg.tag_names = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'img'];
+
+// check if current page is loaded in iframe
+if (inIframe()) {
+  document.head.innerHTML += `
+  <style>
+  h1,h2,h3,h4,p,div {
+    border: 1px solid transparent;
+  }
+  h1:hover,h2:hover,h3:hover,h4:hover,p:hover,img:hover,.current-item {
+    border: 1px solid lightblue; cursor: pointer;
+  }
+  </style>`
+
+  document.body.addEventListener('click', function(e) {
+
+    let target = e.target;
+    let tagname = e.target.tagName.toLowerCase();
+    if (cfg.tag_names.includes(tagname)) {
+
+      var data = {};
+      data.msg = 'clicked';
+
+      if (tagname == 'h1' || tagname == 'h2' || tagname == 'h3' || tagname == 'h4' || tagname == 'h5' || tagname == 'h6') {
+        data.type = 'text';
+        data.value = target.innerText;
+      }
+      if (tagname == 'p') {
+        data.type = 'html';
+        data.value = target.innerHTML;
+      }
+      if (tagname == 'img') {
+        data.type = 'src';
+        data.value = target.src;
+      }
+
+      sendMessage(data);
+
+      document.querySelectorAll('.current-item').forEach(function(e) {
+        e.classList.remove('current-item');
+      })
+      target.classList.add('current-item');
+    }
+
+  })
+
+  /* --- Message Center --- */
+
+  window.addEventListener('message', receiveMessage, false);
+
+  function sendMessage(data) {
+    console.log(data.msg);
+    window.parent.postMessage(JSON.stringify(data), cfg.remote_domain);
+  }
+
+  function receiveMessage(event) {
+
+    let data = JSON.parse(event.data);
+    let cur = document.querySelector('.current-item');
+
+    if (data.msg == 'updated') {
+      if (data.type == 'text') {
+        cur.innerText = data.value;
+      } else if (data.type == 'html') {
+        cur.innerHTML = data.value;
+      } else if (data.type == 'src') {
+        cur.src = data.value;
+      }
+    }
+
+    if (data.msg == 'save') {
+      let html = `
+      <!DOCTYPE html>
+      <html>`
+      html += document.head.outerHTML
+      html += document.body.outerHTML;
+      html += `</html>`
+      console.log(html);
+    }
+
+  }
+
+} // end inIframe
+
+function inIframe() {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+}
